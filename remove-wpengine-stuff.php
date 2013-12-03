f72023bf4f77423ffab4df6d3a2dacb66d2fb665
Toby <toby@themightymo.com>
2013-12-03 10:55:54 -0600

    initial commit

---
+++ remove-wpengine-stuff.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Plugin Name: Dashboard Cleanup by The Mighty Mo! Design Co.
+ * Plugin URI: https://github.com/themightymo/
+ * Description: Removes WPEngine stuff from the dashboard for non-tobys
+ * Author: The Mighty Mo! Design Co. LLC
+ * Author URI: http://www.themightymo.com/
+ * License: GPLv2 (or later)
+ * Version: 1.1
+ */
+ 
+// Remove all evidence of WP Engine from the Dashboard, unless the logged in user is "wpengine" 
+// via http://jeremypry.com/hide-wp-engine-dashboard-extras/
+$user = wp_get_current_user();
+if ( $user->user_login != 'wpengine' && $user->user_login != 'toby' ) {
+        add_action( 'admin_init', 'jpry_remove_menu_pages' );
+        add_action( 'admin_bar_menu', 'jpry_remove_admin_bar_links', 999 );
+}
+ 
+/**
+ * Remove the WP Engine menu page
+ */
+function jpry_remove_menu_pages() {
+        remove_menu_page( 'wpengine-common' );
+}
+ 
+/**
+ * Remove the "WP Engine Quick Links" from the menu bar
+ */
+function jpry_remove_admin_bar_links( $wp_admin_bar ) {
+        $wp_admin_bar->remove_node( 'wpengine_adminbar' );
+}
