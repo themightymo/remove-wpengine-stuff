90f36bd32fc29735ba682d5fce50f1645b564047
Toby <toby@themightymo.com>
2013-12-03 11:22:34 -0600

    Fixed function call

--- remove-wpengine-stuff.php
+++ remove-wpengine-stuff.php
@@ -12,13 +12,15 @@
 // Remove all evidence of WP Engine from the Dashboard, unless the logged in user is "wpengine" 
 // via http://jeremypry.com/hide-wp-engine-dashboard-extras/
 
+require_once(ABSPATH . '/wp-includes/pluggable.php');
 global $current_user;
+get_currentuserinfo();
 
 if ( $current_user->user_login != 'wpengine' && $current_user->user_login != 'toby' ) {
 	add_action( 'admin_init', 'jpry_remove_menu_pages' );
 	add_action( 'admin_bar_menu', 'jpry_remove_admin_bar_links', 999 );
 }
- 
+
 /**
  * Remove the WP Engine menu page
  */
